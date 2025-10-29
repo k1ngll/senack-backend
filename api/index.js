f4d6232d2ab2ca07cea7d4f158aa2c735b106299
Sena <antoniocarlossenajunior@gmail.com>
2025-10-29 00:24:47 -0300

    chore: PACOTES NECESSARIOS PARA O VERSEL
    Parametros adicionados
    -name
    -version
    -main

--- api/index.js
+++ api/index.js
@@ -5,11 +5,12 @@ const cors = require('cors');
 
 const app = express();
 // Adicione isso ao final de api/index.js
+/*
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor da API rodando em http://localhost:${PORT}`);
 });
-
+*/
 
 app.use(cors());
 app.use(express.json());
